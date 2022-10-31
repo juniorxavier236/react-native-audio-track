f3be6e4484045bef1a37f160cec3fb8206a7c79d
DevApp <desenvolvimento@tecnorise.com.br>
2022-10-31 15:05:39 -0300

    correção package json

--- android/src/main/java/com/audiotrack/RNAudioTrackModule.java
+++ android/src/main/java/com/audiotrack/RNAudioTrackModule.java
@@ -1,10 +1,15 @@
 package com.audiotrack;
 
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableArray;
+
+
+import java.lang.Math;
+
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -22,6 +27,7 @@ import android.media.AudioFormat;
 import android.os.Build;
 import android.util.Base64;
 import android.util.Log;
+
 
 public class RNAudioTrackModule extends ReactContextBaseJavaModule {
 
