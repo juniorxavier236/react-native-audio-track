268153fc5749c0b5c4937e7c8958285921f931ba
DevApp <desenvolvimento@tecnorise.com.br>
2022-10-31 15:11:19 -0300

    a

--- android/src/main/java/com/audiotrack/RNAudioTrackPackage.java
+++ android/src/main/java/com/audiotrack/RNAudioTrackPackage.java
@@ -1,23 +1,23 @@
 package com.audiotrack;
 
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.facebook.react.bridge.JavaScriptModule;
 public class RNAudioTrackPackage implements ReactPackage {
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      List<NativeModule> modules = new ArrayList<>();
-      modules.add(new RNAudioTrackPackage(reactContext));
-      return modules;
+      return Arrays.<NativeModule>asList(new RNAudioTrackModule(reactContext));
+    }
+
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
     }
 
     @Override
